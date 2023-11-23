2195d441a7b6bdc8b42475257820b158d81ce500
pixeclouds <earthbit@protonmail.com>
2023-11-23 00:51:53 -1200

    update home msg

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(userRouter)
 
 app.get('/', (req, res) => {
-    res.send('app running')
+    res.json({"message": "farmer support API is up and running"})
 })
 
 // database connection
