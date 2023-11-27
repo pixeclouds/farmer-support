74611bef8e694a6de84addef7933b96ecca78dc3
pixeclouds <earthbit@protonmail.com>
2023-11-26 22:25:55 -1200

    handled auth error

--- src/middlewares/authorize.js
+++ src/middlewares/authorize.js
@@ -3,10 +3,11 @@ const { getUser } = require("../modules/farmer/repository")
 
 //check if user is signed in
 exports.userAuthorized = async (req, res, next) => {
-    let Authorization = req.get("Authorization")
-    console.log(Authorization)
-    let token = Authorization.split(" ")[1]
+
     try {
+        let Authorization = req.get("Authorization")
+        console.log(Authorization)
+        let token = Authorization.split(" ")[1]
         if(!token){
             throw Error
         }
