52760b7e273ac1a681f262021614f7a1162c33e3
pixeclouds <earthbit@protonmail.com>
2023-11-26 22:12:42 -1200

    fix

--- src/modules/forecast/controller.js
+++ src/modules/forecast/controller.js
@@ -5,7 +5,9 @@ const userRepository = require('../../modules/farmer/repository')
 exports.weatherForecast = async (req, res) => {
     try {
         let { location } = await userRepository.getUser(req.email)
+        console.log(location)
         let {latitude, longitude } = await getLatLong(location)
+        console.log(latitude , longitude )
         let weather = await getWeatherForecast(latitude, longitude)
 
         res.status(200).json(weather)
